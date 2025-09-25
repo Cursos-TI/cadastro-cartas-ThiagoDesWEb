f3ada14c50f5a18a1093bd891389eb8f3d70447a
Thiago Moura <thiagomooura@gmail.com>
2025-09-25 15:47:50 +0000

    .

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,13 @@ int main() {
 
   //Variaveis Globais
 
-  char estado1, estado2 ; // Uma letra para representar o estado
-  char codigo1[4], codigo2[4]; // Código da cidade 1 e cidade 2 (ex: A01, A02)
-  char nome1[50], nome2[50]; // Nome da cidade 1 e cidade 2
-  int populacao1, populacao2; // População da cidade 1 e cidade 2 
-  int pontosTuristicos1, pontosTuristicos2; // Total de pontos turisticos cidade 1 e cidade 2
-  float area1, area2; // Área em km² da cidade 1 e cidade 2
-  float pib1, pib2; // PIB total da cidade
+  char estado1, estado2;  //Uma letra para representar o estado
+  char codigo1[4], codigo2[4];  // Código da cidade 1 e cidade 2 (ex: A01, A02)
+  char nome1[50], nome2[50];  // Nome da cidade 1 e cidade 2
+  int populacao1, populacao2;  // População da cidade 1 e cidade 2 
+  int pontosTuristicos1, pontosTuristicos2;  // Total de pontos turisticos cidade 1 e cidade 2
+  float area1, area2;  // Área em km² da cidade 1 e cidade 2
+  float pib1, pib2;  // PIB total da cidade
 
   //Entrada de dados
 
